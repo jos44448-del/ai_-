b110e41fe51cd747a095b9cafc5511796889597b
jos44448-del <jos44448@gmail.com>
2025-11-04 14:07:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기')
 st.text_input('이름')
-if st.selectbox('좋아하는 음식:',['초밥','국밥','김치찌개'])
+st.selectbox('좋아하는 음식:',['초밥','국밥','김치찌개'])
 if st.button('인사말 생성'):
   st.info(name+'님! 안녕하세요')
   st.warning(menu+'을(를) 좋아하시나봐요, 저도 좋아해요')
