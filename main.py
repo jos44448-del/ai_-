382a9c2ef554be1e6ac50d5d479ebb1eb553ef44
jos44448-del <jos44448@gmail.com>
2025-11-04 14:11:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ menu=st.selectbox('좋아하는 음식:',['초밥','국밥','김치찌개'])
 if st.button('인사말 생성'):
   st.info(name+'님! 안녕하세요')
   st.warning(menu+'을(를) 좋아하시나봐요, 저도 좋아해요')
+  st.error('반갑습니다!!')
+  st.balloons()
